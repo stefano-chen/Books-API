36e80ddfe254667dc9e15ae16f1c16cb1f430c8d
stefano-chen <stefanochen01@gmail.com>
2024-11-21 12:04:29 +0100

    dockerized the server and database

---
+++ database/scripts/mongo-init.js
@@ -0,0 +1,144 @@
+db = connect( 'mongodb://localhost:27017/booksDB' );
+
+db.books.insertMany([
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    },
+    {
+        "id": "1",
+        "title": "Stefano",
+        "description": "Stefano"
+    }
+]);
